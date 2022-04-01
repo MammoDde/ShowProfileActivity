9ef58385f3dd10ac34a2e8253dad3b9b20517f12
Giacomo <giacomo.inghilleri@gmail.com>
2022-04-01 16:24:03 +0200

    button works

--- app/src/main/java/it/polito/showprofileactivity/EditProfileActivity.kt
+++ app/src/main/java/it/polito/showprofileactivity/EditProfileActivity.kt
@@ -1,11 +1,22 @@
 package it.polito.showprofileactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import android.widget.Toast.*
 
 class EditProfileActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+        var editButton: ImageButton = findViewById(R.id.imageButton2)
+        editButton.setOnClickListener {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
     }
 }
