1c61c8f4b7c1799362999280d90f806ebf3b408b
Giacomo <giacomo.inghilleri@gmail.com>
2022-04-01 21:39:06 +0200

    popup menu

--- app/src/main/java/it/polito/showprofileactivity/EditProfileActivity.kt
+++ app/src/main/java/it/polito/showprofileactivity/EditProfileActivity.kt
@@ -3,10 +3,13 @@ package it.polito.showprofileactivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
 import android.widget.Toast.*
+import androidx.appcompat.widget.PopupMenu
 
 class EditProfileActivity : AppCompatActivity() {
 
@@ -14,9 +17,32 @@ class EditProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
         var editButton: ImageButton = findViewById(R.id.imageButton2)
+
+
         editButton.setOnClickListener {
-            val i = Intent(this, MainActivity::class.java)
-            startActivity(i)
-        }
+            val popupMenu = PopupMenu(this, it)
+            popupMenu.setOnMenuItemClickListener { item ->
+                when (item.itemId){
+                    R.id.menu_open_website -> {
+                        Toast.makeText(this, "Camera!", Toast.LENGTH_LONG).show()
+                        true
+                    }
+                    R.id.menu_show_toast -> {
+                        Toast.makeText(this, "Gallery!", Toast.LENGTH_LONG).show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+            popupMenu.inflate(R.menu.photo_menu)
+            popupMenu.show()
     }
+
+
+}
+
+
+
+
 }
