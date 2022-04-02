9aa74a4c1f57f0525898a6ff3a628db936e79095
MammoDde <mammo3dade@gmail.com>
2022-04-02 12:42:08 +0200

    added: point 5 started

--- app/src/main/java/it/polito/showprofileactivity/EditProfileActivity.kt
+++ app/src/main/java/it/polito/showprofileactivity/EditProfileActivity.kt
@@ -3,6 +3,9 @@ package it.polito.showprofileactivity
 import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore
+import android.text.Editable
+import android.view.View
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +18,32 @@ class EditProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
         var editButton: ImageButton = findViewById(R.id.imageButton2)
+
+        var name: String? = intent.getStringExtra("Name")
+        var nick: String? = intent.getStringExtra("Nickname")
+        var email: String? = intent.getStringExtra("email")
+        var loc: String? = intent.getStringExtra("location")
+        var skill1: String? = intent.getStringExtra("skill1")
+        var skill2: String? = intent.getStringExtra("skill2")
+        var desc1: String? = intent.getStringExtra("description1")
+        var desc2: String? = intent.getStringExtra("description2")
+        var ed1: EditText = findViewById<EditText>(R.id.editTextTextPersonName8)
+        var ed2: EditText = findViewById<EditText>(R.id.editTextTextPersonName9)
+        var ed3: EditText = findViewById<EditText>(R.id.editTextTextEmailAddress3)
+        var ed4: EditText = findViewById<EditText>(R.id.editTextTextPersonName10)
+        var ed5: EditText = findViewById<EditText>(R.id.editTextTextPersonName11)
+        var ed6: EditText = findViewById<EditText>(R.id.editTextTextPersonName16)
+        var ed7: EditText = findViewById<EditText>(R.id.editTextTextMultiLine)
+        var ed8: EditText = findViewById<EditText>(R.id.editTextTextMultiLine2)
+
+        ed1.setText(name)
+        ed2.setText(nick)
+        ed3.setText(email)
+        ed4.setText(loc)
+        ed5.setText(skill1)
+        ed6.setText(skill2)
+        ed7.setText(desc1)
+        ed8.setText(desc2)
 
 
         editButton.setOnClickListener {
@@ -34,6 +63,7 @@ class EditProfileActivity : AppCompatActivity() {
                     }
                     else -> false
                 }
+
             }
 
             popupMenu.inflate(R.menu.photo_menu)
--- app/src/main/java/it/polito/showprofileactivity/MainActivity.kt
+++ app/src/main/java/it/polito/showprofileactivity/MainActivity.kt
@@ -25,6 +25,14 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.icon -> {
                 val i = Intent(this,EditProfileActivity::class.java)
+                i.putExtra("Name",getString(R.string.mario_rossi))
+                i.putExtra("Nickname",getString(R.string.mario22))
+                i.putExtra("email",getString(R.string.mario_rossi_polito_it))
+                i.putExtra("location",getString(R.string.corso_duca_degli_abruzzi_torino))
+                i.putExtra("skill1",getString(R.string.english))
+                i.putExtra("skill2",getString(R.string.babysitting))
+                i.putExtra("description1",getString(R.string.a_good_english_teacher))
+                i.putExtra("description2",getString(R.string.i_take_care_of_baby))
                 startActivity(i)
                 true
             }
