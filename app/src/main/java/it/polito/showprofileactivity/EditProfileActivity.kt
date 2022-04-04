14d3a49c33bdf5681e8c9ff439d73e1918297f32
Giacomo <giacomo.inghilleri@gmail.com>
2022-04-04 15:17:47 +0200

    camera

--- app/src/main/java/it/polito/showprofileactivity/EditProfileActivity.kt
+++ app/src/main/java/it/polito/showprofileactivity/EditProfileActivity.kt
@@ -1,16 +1,26 @@
 package it.polito.showprofileactivity
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
-import android.text.Editable
-import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
-import android.widget.Toast
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.PopupMenu
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
+private const val REQUEST_IMAGE_CAPTURE = 1
+private const val name = "photoG20.jpg"
+private lateinit var photoFile: File
+private lateinit var currentPhotoPath: String
 
 class EditProfileActivity : AppCompatActivity() {
 
@@ -51,8 +61,28 @@ class EditProfileActivity : AppCompatActivity() {
             popupMenu.setOnMenuItemClickListener { item ->
                 when (item.itemId){
                     R.id.menu_open_camera -> {
-                        val photoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                        startActivity(photoIntent);
+                        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                            // Ensure that there's a camera activity to handle the intent
+                            takePictureIntent.resolveActivity(packageManager)?.also {
+                                // Create the File where the photo should go
+                                val photoFile: File? = try {
+                                    createImageFile()
+                                } catch (ex: IOException) {
+                                    // Error occurred while creating the File
+                                    null
+                                }
+                                // Continue only if the File was successfully created
+                                photoFile?.also {
+                                    val photoURI: Uri = FileProvider.getUriForFile(
+                                        this,
+                                        "com.example.android.fileprovider",
+                                        it
+                                    )
+                                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                                }
+                            }
+                        }
                         true
                     }
                     R.id.menu_open_gallery -> {
@@ -68,10 +98,33 @@ class EditProfileActivity : AppCompatActivity() {
 
             popupMenu.inflate(R.menu.photo_menu)
             popupMenu.show()
+        }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            val image = findViewById<ImageView>(R.id.imageView)
+            image.setImageBitmap(imageBitmap)
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
 
-}
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
 
 
 
